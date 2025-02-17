aafec018a31bd85e66c8271a1d0ef322868bc2af
Aurekin <152402837+Aurekin@users.noreply.github.com>
2025-02-17 14:13:57 +0100

    assistant.ps1

--- assistant.ps1
+++ assistant.ps1
@@ -1,2 +1,2 @@
-$process = Start-Process python -ArgumentList '"C:\Users\Aurelien\Documents\Logiciels\uiai\main.py"' -WindowStyle Hidden
+$process = Start-Process python -ArgumentList '"C:\chemin vers le fichier python main"' -WindowStyle Hidden
 exit
